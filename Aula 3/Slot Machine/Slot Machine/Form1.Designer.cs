9f59984881c02cc0ae82fd7710fbdd2f0071e063
Mário Teixeira <1090626@isep.ipp.pt>
2015-10-08 23:36:57 +0100

    Adicionado versão 2 do Slot Machine com imagens invês de números. Adicionado versão inicial do stopwatch para a geração aleatória de imagens.

---
+++ Aula 3/Slot Machine/Slot Machine/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Slot_Machine
+{
+    partial class SlotMachine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.saldoLabel = new System.Windows.Forms.Label();
+            this.apostaLabel = new System.Windows.Forms.Label();
+            this.saldoValor = new System.Windows.Forms.Label();
+            this.apostaBox = new System.Windows.Forms.NumericUpDown();
+            this.Box1 = new System.Windows.Forms.PictureBox();
+            this.Box2 = new System.Windows.Forms.PictureBox();
+            this.Box3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.apostaBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(138, 426);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            this.result.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.ForeColor = System.Drawing.Color.Red;
+            this.result.Location = new System.Drawing.Point(79, 346);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(240, 23);
+            this.result.TabIndex = 4;
+            this.result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // saldoLabel
+            // 
+            this.saldoLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.saldoLabel.ForeColor = System.Drawing.Color.Black;
+            this.saldoLabel.Location = new System.Drawing.Point(22, 386);
+            this.saldoLabel.Name = "saldoLabel";
+            this.saldoLabel.Size = new System.Drawing.Size(72, 23);
+            this.saldoLabel.TabIndex = 5;
+            this.saldoLabel.Text = "Saldo: ";
+            this.saldoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // apostaLabel
+            // 
+            this.apostaLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.apostaLabel.ForeColor = System.Drawing.Color.Black;
+            this.apostaLabel.Location = new System.Drawing.Point(208, 386);
+            this.apostaLabel.Name = "apostaLabel";
+            this.apostaLabel.Size = new System.Drawing.Size(67, 23);
+            this.apostaLabel.TabIndex = 6;
+            this.apostaLabel.Text = "Saldo: ";
+            this.apostaLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // saldoValor
+            // 
+            this.saldoValor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.saldoValor.ForeColor = System.Drawing.Color.Black;
+            this.saldoValor.Location = new System.Drawing.Point(100, 386);
+            this.saldoValor.Name = "saldoValor";
+            this.saldoValor.Size = new System.Drawing.Size(90, 23);
+            this.saldoValor.TabIndex = 8;
+            this.saldoValor.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // apostaBox
+            // 
+            this.apostaBox.Location = new System.Drawing.Point(287, 387);
+            this.apostaBox.Name = "apostaBox";
+            this.apostaBox.Size = new System.Drawing.Size(75, 20);
+            this.apostaBox.TabIndex = 9;
+            // 
+            // Box1
+            // 
+            this.Box1.BackColor = System.Drawing.Color.White;
+            this.Box1.BackgroundImage = global::Slot_Machine.Properties.Resources.orange;
+            this.Box1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Box1.Location = new System.Drawing.Point(79, 169);
+            this.Box1.Name = "Box1";
+            this.Box1.Size = new System.Drawing.Size(69, 78);
+            this.Box1.TabIndex = 10;
+            this.Box1.TabStop = false;
+            // 
+            // Box2
+            // 
+            this.Box2.BackColor = System.Drawing.Color.White;
+            this.Box2.BackgroundImage = global::Slot_Machine.Properties.Resources.orange;
+            this.Box2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Box2.Location = new System.Drawing.Point(160, 169);
+            this.Box2.Name = "Box2";
+            this.Box2.Size = new System.Drawing.Size(69, 78);
+            this.Box2.TabIndex = 11;
+            this.Box2.TabStop = false;
+            // 
+            // Box3
+            // 
+            this.Box3.BackColor = System.Drawing.Color.White;
+            this.Box3.BackgroundImage = global::Slot_Machine.Properties.Resources.orange;
+            this.Box3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Box3.Location = new System.Drawing.Point(239, 169);
+            this.Box3.Name = "Box3";
+            this.Box3.Size = new System.Drawing.Size(69, 78);
+            this.Box3.TabIndex = 12;
+            this.Box3.TabStop = false;
+            // 
+            // SlotMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Slot_Machine.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(388, 480);
+            this.Controls.Add(this.Box3);
+            this.Controls.Add(this.Box2);
+            this.Controls.Add(this.Box1);
+            this.Controls.Add(this.apostaBox);
+            this.Controls.Add(this.saldoValor);
+            this.Controls.Add(this.apostaLabel);
+            this.Controls.Add(this.saldoLabel);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button1);
+            this.Name = "SlotMachine";
+            this.Text = "Slot Machine";
+            ((System.ComponentModel.ISupportInitialize)(this.apostaBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Box3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label saldoLabel;
+        private System.Windows.Forms.Label apostaLabel;
+        private System.Windows.Forms.Label saldoValor;
+        private System.Windows.Forms.NumericUpDown apostaBox;
+        private System.Windows.Forms.PictureBox Box1;
+        private System.Windows.Forms.PictureBox Box2;
+        private System.Windows.Forms.PictureBox Box3;
+    }
+}
+
---
+++ Aula 3/Slot Machine/Slot Machine/Form1.cs
@@ -0,0 +1,146 @@
+﻿using Slot_Machine.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slot_Machine
+{
+    public partial class SlotMachine : Form
+    {
+        int saldo = 20; //Saldo inicial
+
+        public SlotMachine()
+        {
+            InitializeComponent();
+            saldoValor.Text = saldo.ToString();           
+        }
+
+        /*
+        *   Função para gerar imagens aleatóriamente com um efeito 
+        */
+        private void randomImageEffect()
+        {
+            Random random = new Random();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            while (sw.Elapsed < TimeSpan.FromSeconds(5))
+            {
+                var rand_number1 = random.Next(0, 9);
+                var rand_number2 = random.Next(0, 9);
+                var rand_number3 = random.Next(0, 9);
+                setBoxImage(rand_number1, Box1);
+                setBoxImage(rand_number2, Box2);
+                setBoxImage(rand_number3, Box3);
+            }
+            sw.Stop();
+        }
+
+        /*
+        *   Função para associar o numero gerado aleatóriamente a uma imagem.
+        */
+        private void setBoxImage(int boxNumber, PictureBox box)
+        {
+            Console.WriteLine(boxNumber);
+            if(boxNumber == 1)
+            {
+                box.BackgroundImage = Resources.apple;
+            }
+            if (boxNumber == 2)
+            {
+                box.BackgroundImage = Resources.cherry;
+            }
+            if (boxNumber == 3)
+            {
+                box.BackgroundImage = Resources.banana;
+            }
+            if (boxNumber == 4)
+            {
+                box.BackgroundImage = Resources.grapes;
+            }
+            if (boxNumber == 5)
+            {
+                box.BackgroundImage = Resources.lemon;
+            }
+            if (boxNumber == 6)
+            {
+                box.BackgroundImage = Resources.melon;
+            }
+            if (boxNumber == 7)
+            {
+                box.BackgroundImage = Resources.orange; //vence
+            }
+            if (boxNumber == 8)
+            {
+                box.BackgroundImage = Resources.apple;
+            }
+            if (boxNumber == 9)
+            {
+                box.BackgroundImage = Resources.cherry;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int aposta = (int)apostaBox.Value;  //Obter valor da aposta
+
+            result.Text = "";   //Limpa a caixa de texto do resultado
+
+            if (aposta > 0 && aposta <= saldo) //Apenas é aceite a aposta se o valor for menor ou igual ao saldo
+            {
+                saldo = saldo - aposta; //retira o valor da aposta ao saldo
+
+                //gera tres numeros aleatóriamente
+                var box1_number = random.Next(0, 9);
+                var box2_number = random.Next(0, 9);
+                var box3_number = random.Next(0, 9);
+
+                randomImageEffect();
+
+                setBoxImage(box1_number, Box1);
+                setBoxImage(box2_number, Box2);
+                setBoxImage(box3_number, Box3);
+
+                if (box1_number == 7 && box2_number == 7 && box3_number == 7)    //aposta decuplicada
+                {
+                    aposta = aposta * 10;
+                    saldo = saldo + aposta;
+                    result.Text = "Parabéns! Ganhou Jackpot.";
+                }
+                else if ((box1_number == 7 && box2_number == 7) || (box1_number == 7 && box3_number == 7) || (box2_number == 7 && box3_number == 7))  //aposta triplicada
+                {
+                    aposta = aposta * 3;
+                    saldo = saldo + aposta;
+                    result.Text = "Parabéns! Aposta triplicada.";
+                }
+                else if (box1_number == 7 || box2_number == 7 || box3_number == 7)  //aposta dobrada
+                {
+                    aposta = aposta * 2;
+                    saldo = saldo + aposta;
+                    result.Text = "Parabéns! Aposta dobrada.";
+                }
+
+                saldoValor.Text = saldo.ToString(); //atualiza o valor do saldo
+            }
+            else
+            {
+                if (aposta == 0)
+                {
+                    result.Text = "A aposta tem que ser maior que 0.";
+                }
+                else
+                {
+                    result.Text = "Não tem saldo suficiente para jogar.";
+                }                
+            }         
+        }
+    }
+}
