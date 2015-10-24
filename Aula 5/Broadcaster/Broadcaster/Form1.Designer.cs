f07b3415d5badfe93582b19f29fb21e52bb778ac
Mário Teixeira <1090626@isep.ipp.pt>
2015-10-24 22:28:28 +0100

    Adicionado versão inicial do exercício Broadcaster

---
+++ Aula 5/Broadcaster/Broadcaster/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Broadcaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LocalCamera = new System.Windows.Forms.PictureBox();
+            this.LCBtn = new System.Windows.Forms.Button();
+            this.LCconfig = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LocalCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(764, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(104, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(88, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(598, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "INDES - BROADCASTER";
+            // 
+            // LocalCamera
+            // 
+            this.LocalCamera.Location = new System.Drawing.Point(28, 109);
+            this.LocalCamera.Name = "LocalCamera";
+            this.LocalCamera.Size = new System.Drawing.Size(143, 115);
+            this.LocalCamera.TabIndex = 2;
+            this.LocalCamera.TabStop = false;
+            // 
+            // LCBtn
+            // 
+            this.LCBtn.Location = new System.Drawing.Point(28, 240);
+            this.LCBtn.Name = "LCBtn";
+            this.LCBtn.Size = new System.Drawing.Size(75, 51);
+            this.LCBtn.TabIndex = 3;
+            this.LCBtn.Text = "Local Camera";
+            this.LCBtn.UseVisualStyleBackColor = true;
+            this.LCBtn.Click += new System.EventHandler(this.LCBtn_Click);
+            // 
+            // LCconfig
+            // 
+            this.LCconfig.FormattingEnabled = true;
+            this.LCconfig.Location = new System.Drawing.Point(28, 455);
+            this.LCconfig.Name = "LCconfig";
+            this.LCconfig.Size = new System.Drawing.Size(226, 21);
+            this.LCconfig.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Broadcaster.Properties.Resources.wallpaper1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(764, 488);
+            this.Controls.Add(this.LCconfig);
+            this.Controls.Add(this.LCBtn);
+            this.Controls.Add(this.LocalCamera);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "INDES - Broadcaster";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LocalCamera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox LocalCamera;
+        private System.Windows.Forms.Button LCBtn;
+        private System.Windows.Forms.ComboBox LCconfig;
+    }
+}
+
---
+++ Aula 5/Broadcaster/Broadcaster/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Broadcaster
+{
+    public partial class Form1 : Form
+    {
+        private bool DeviceExist = false;
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource = null;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            getCamList();
+        }
+
+        //Função para obter a lista de cameras
+        private void getCamList()
+        {
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                LCconfig.Items.Clear();
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                DeviceExist = true;
+                foreach (FilterInfo device in videoDevices)
+                {
+                    LCconfig.Items.Add(device.Name);
+                }
+                LCconfig.SelectedIndex = 0; //Usar a primeira camera da lista
+            }
+            catch (ApplicationException)
+            {
+                DeviceExist = false;
+                LCconfig.Items.Add("No capture device on your system");
+            }
+        }
+
+        private void LCBtn_Click(object sender, EventArgs e)
+        {
+         /*  if (DeviceExist)
+           {
+                videoSource = new VideoCaptureDevice(videoDevices[LCconfig.SelectedIndex].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(LocalCamera);
+                    CloseVideoSource();
+                    videoSource.DesiredFrameSize = new Size(160, 120);
+                    videoSource.DesiredFrameRate = 10;
+                    videoSource.Start();
+                    //label2.Text = "Device running...";
+                    //start.Text = "&Stop";
+                    //timer1.Enabled = true;
+           }
+           else
+           {
+                //         label2.Text = "Error: No Device selected.";
+                }
+            }
+            else
+            {
+                if (videoSource.IsRunning)
+                {
+                    timer1.Enabled = false;
+                    CloseVideoSource();
+                    label2.Text = "Device stopped.";
+                    start.Text = "&Start";
+                }
+            }*/
+        }
+    }
+}
